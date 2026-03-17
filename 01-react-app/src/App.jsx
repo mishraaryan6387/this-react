45fbc4c111964f48bcb908b92c773081bcd1a599
Aryan <mishraaryan6387@gmail.com>
2026-03-17 12:48:08 +0530

    Add React app project (01-react-app) with all files including Chai.jsx

---
+++ 01-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai';
+
+function App() {
+  return (
+   <><h1> chai aur react with vite 
+    </h1>
+     <Chai />
+     <p> purqa hua </p>
+     </> 
+  );
+  }
+
+
+export default App;
---
+++ 01-react-app/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai() {
+
+return (<h3> chai aur react with vite</h3>);
+
+}
+export default Chai ;
